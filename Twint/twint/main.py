b367095a171800af14a7c15d67503793f7cf923e
dodo <k1013726@gmail.com>
2023-03-26 22:46:11 +0800

    update

--- Twint/twint/main.py
+++ Twint/twint/main.py
@@ -1,19 +1,20 @@
 import twint
 
 c = twint.Config()
-#Bitcoin News
-c.Username='BTCTN'
 c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
+#搜尋使用者名稱貼文
+# c.Username='BTCTN'
 
-c.Format = "ID {id} | Username {username}"
-
-# c.Lang='en'
-# c.Since='2014-10-27'
-# c.Until='2023-2-28'            
+c.Search='bitcion'
 c.Store_csv = True
 c.Output = "BitcionSearch.csv"
-c.Count=True
-c.Limit=20
+
+
+# c.Since='2023-2-26'
+# c.Until='2023-2-20'        
+# c.Lang='en'
+# c.Count=True
+
 # Run
 twint.run.Search(c)
